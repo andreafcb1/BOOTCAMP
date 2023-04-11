d23d19041d33dbe62ef485f9bff63c20c97dc567
andreafcb1 <andrea_garcillan@hotmail.com>
2023-04-11 17:57:27 +0200

    Actualización Functions

---
+++ JS_EXERCISES/JS_FUNCTIONS/01.js
@@ -0,0 +1,8 @@
+// -------------- BUSCAR EL MÁXIMO------------------
+
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+    numberOne > numberTwo ? console.log(numberOne) : console.log(numberTwo)
+  }
+  sum(78 , 906)
--- JS_EXERCISES/JS_FUNCTIONS/02.js
+++ JS_EXERCISES/JS_FUNCTIONS/02.js
@@ -1,5 +1,12 @@
 // Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
-// const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
-// function findLongestWord(param) {
-//   // insert code
-// }
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+const findLongestWord = (array) => {
+    let largestWord = "";
+   array.forEach(avenger => {
+    largestWord = avenger.length > largestWord.length ? avenger : largestWord;
+   });
+   return largestWord;
+ }
+
+ console.log(findLongestWord(avengers))
