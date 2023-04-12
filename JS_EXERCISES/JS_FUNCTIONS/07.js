f02b246ede1680ee194d7c2ea738e3e7df3e2bc5
andreafcb1 <andrea_garcillan@hotmail.com>
2023-04-12 22:50:43 +0200

    l

---
+++ JS_EXERCISES/JS_FUNCTIONS/07.js
@@ -0,0 +1,27 @@
+// ---------------- BUSCADOR DE NOMBRES ----------------
+
+// Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. 
+
+const nameFinder = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+  ];
+  const finderName = (array,word) => {
+    array.forEach((name, index) => {
+        word == name ? console.log(true, index) : console.log(false)
+    })
+  }
+  finderName(nameFinder,"Peggy");
+
+  // ACABAR
+
+  
