5258d976cd49dd2ce48987ddc5a1628bf22bdd6f
andreafcb1 <andrea_garcillan@hotmail.com>
2023-04-17 14:23:56 +0200

    Spread Operator

--- JS_EXERCISES/JS_ES6/03.js
+++ JS_EXERCISES/JS_ES6/03.js
@@ -6,6 +6,9 @@ const list = [32, 54, 21, 64, 75, 43]
 
 let listCopy = [...list];
 
+console.log(list);
+console.log(listCopy);
+
 // 3.2 Dado el siguiente objeto, crea una copia usando spread operators.
 
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
@@ -31,5 +34,10 @@ const fusedToy = {...toy1, ...toy1Update};
 // 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
 // pero sin editar el array inicial. De nuevo, usando spread operatos.
 
-//const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
 
+let colorsCopy = [...colors];
+
+colors.splice(1,1);
+
+console.log(colors);
