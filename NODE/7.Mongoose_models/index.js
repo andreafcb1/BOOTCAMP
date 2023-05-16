1e938ba267cb24b091a004a5f11a107d02dd3471
andreafcb1 <andrea_garcillan@hotmail.com>
2023-05-16 18:03:22 +0200

    modelo mongoose

--- NODE/7.Mongoose_models/index.js
+++ NODE/7.Mongoose_models/index.js
@@ -1,3 +1,14 @@
+//crear servidor web
 const { connect } = require("./src/utils/db");
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
+//contectar con la DB
 connect();
+const app = express();
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT http://localhost:${PORT}`);
+});
